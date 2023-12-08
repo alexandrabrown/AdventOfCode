3f28d078f49ee171a36f2961cc314664f6452716
Alexandra Brown <alexbro@umich.edu>
2023-12-08 10:46:53 -0500

    Read in input file

---
+++ day2/day2.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatalf("failed to open")
+
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var text []string
+
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+
+	file.Close()
+
+	for _, each_ln := range text {
+		fmt.Println(each_ln)
+	}
+}
