4f2ad3ad4c057f526b0b0b4d4b968d983c0cc180
Alexandra Antosh <alexandra.antosh@learninga-z.com>
2024-12-06 14:08:28 -0500

    Implement day 2 part 1

---
+++ 2024/day2/day2part2.py
@@ -0,0 +1,33 @@
+from itertools import pairwise
+
+def is_strictly_increasing(my_list):
+    if all(a < b for a, b in pairwise(my_list)):
+        return True
+    else:
+        return False
+
+def is_strictly_decreasing(my_list):
+    if all(a > b for a, b in pairwise(my_list)):
+        return True
+    else:
+        return False
+    
+def check_differences(my_list):
+    if all(abs(a-b) >=1 and abs(a-b) <=3 for a, b in pairwise(my_list)):
+        return True
+    else:
+        return False
+
+def main():
+    numSafe = 0
+    with open('input.txt', 'r') as file:
+        for line in file:
+            my_list = [float(x) for x in line.split()]
+            if ((is_strictly_increasing(my_list) or is_strictly_decreasing(my_list)) and check_differences(my_list)):
+                numSafe += 1
+
+    print(numSafe)
+
+
+if __name__ == "__main__":
+    main()
